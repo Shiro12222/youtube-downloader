70c8b42a0deff160416891097d0f18a1f5ead7c4
Shiro12222 <leo12190818@gmail.com>
2024-12-01 21:32:50 +0800

    Result board and embed youtube preview

--- public/js/main.js
+++ public/js/main.js
@@ -39,6 +39,41 @@ function setPageScale() {
   wrapper.style.transformOrigin = 'top center';
 }
 
+function convertYouTubeUrl(url) {
+  return url.replace('watch?v=', 'embed/');
+}
+
+async function getInfo(){
+  try {
+    const youtubeLink = document.getElementById("inputForm").value;
+    const response = await fetch('/youtube/getTitle', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url: youtubeLink })
+    });
+
+    const videoInfo = await response.json();
+    console.log(videoInfo);
+    const descriptionDetails = document.getElementById("descriptionDetails");
+    const descriptionTitle = document.getElementById("descriptionTitle");
+    const description = document.getElementById("description");
+    const youtubePreview = document.getElementById("youtubePreview");
+    const embedYoutubeLink = convertYouTubeUrl(videoInfo.youtubeLink);
+
+    youtubePreview.src = embedYoutubeLink;
+    console.log(embedYoutubeLink);
+    descriptionTitle.textContent = videoInfo.title;
+    descriptionDetails.textContent = "\n" + videoInfo.description + "\n\n";
+    description.style.opacity = "1";
+    descriptionTitle.style.opacity = "1";
+    descriptionDetails.style.opacity = "1";
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
 async function handleDownload(){
   try {
     const youtubeLink = document.getElementById("inputForm").value;
@@ -49,12 +84,13 @@ async function handleDownload(){
         },
         body: JSON.stringify({ url: youtubeLink })
     });
-    const data = await response.json();
-    console.log(data);
+    const download = await response.json();
   } catch (error) {
     console.log("Error:", error);
   }
 }
+
+
 
 window.addEventListener('resize', setPageScale);
 window.addEventListener('load', setPageScale);
@@ -67,7 +103,12 @@ document.getElementById("animateButton").onclick = function() {
 document.getElementById("inputForm").addEventListener('keypress', function(event) {
   if (event.key === 'Enter') {
       event.preventDefault();
+      getInfo();
       handleDownload();
   }
 });
-document.getElementById("downloadButton").onclick = handleDownload;
+
+document.getElementById("downloadButton").onclick = () => {
+  getInfo();
+  handleDownload();
+};
--- routes/youtube.js
+++ routes/youtube.js
@@ -1,22 +1,52 @@
+const { clear } = require('console');
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 
 router.post('/download', async (req, res) => {
-  console.log("can receive")
+  console.log("can receive");
   try{
     const youtubeUrl = req.body.url;
 
     if (!ytdl.validateURL(youtubeUrl)){
-        return res.status(500).send("Invalid link!");
-    } else {
-        console.log("Link Ok");
-    }
-  } catch {
-    console.log("something error");
+      return res.status(500).send("Invalid Youtube URL");
+    } 
+
+    const options = {
+      quality: "highestvideo", 
+      filter: "videoandaudio",
+    };
+    // const youtubeInfo = await ytdl.getInfo(youtubeUrl);
+    // const downURL = ytdl(youtubeUrl, {format: 'mp4'}).pipe(res);
+
+    console.log(details);
+    res.status(200).send(details);
+  } catch (err){
+    console.log("something error", err.message);
   }
-  
+});
+
+router.post('/getTitle', async (req, res) => {
+  try {
+    const youtubeUrl = req.body.url;
+
+    if(!ytdl.validateURL(youtubeUrl)){
+      return res.status(500).send("Invalid Youtube URL");
+    }
+
+    const youtubeInfo = await ytdl.getInfo(youtubeUrl);
+    const youtubeTitle = youtubeInfo.videoDetails.title;
+    const youtubeDescription = youtubeInfo.videoDetails.description;
+
+    res.status(200).json({
+      title: youtubeTitle,
+      description: youtubeDescription,
+      youtubeLink: youtubeUrl
+    });
+  } catch (err) {
+    console.log("something error", err.message);
+  }
 });
 
 module.exports = router;
